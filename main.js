743d899100972af7602af6232df85c04b12a7de7
DidierVanspauwen <113337439+DidierVanspauwen@users.noreply.github.com>
2022-10-03 13:16:03 +0200

    s

---
+++ main.js
@@ -0,0 +1,42 @@
+//Show hidden text//
+function sport() {
+    var x = document.getElementById("sport");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function duiken() {
+    var x = document.getElementById("duiken");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+  function ShowRadioButtonDiv (IdBaseName, NumberOfButtons) {
+    for (x=1;x<=NumberOfButtons;x++) {
+        CheckThisButton = IdBaseName + x;
+        ThisDiv = IdBaseName + x +'Div';
+    if (document.getElementById(CheckThisButton).checked) {
+        document.getElementById(ThisDiv).style.display = "block";
+        }
+    else {
+        document.getElementById(ThisDiv).style.display = "none";
+        }
+    }
+    return false;
+}
